9e9671ed9ce9dd465964dbc1057a498c3f29736f
Juan C. Luna H. <jcluna.oficial@gmail.com>
2017-02-22 00:28:17 -0600

    Limpieza y Conversión String a mayúsculas (upper) y minúsculas (lower)

--- UString.cs
+++ UString.cs
@@ -2,5 +2,43 @@
 {
     public static class UString
     {
+        /// <summary>
+        /// Limpiar y truncar un string
+        /// </summary>
+        /// <param name="value">String a procesar</param>
+        /// <param name="length">Longitud a truncar el String</param>
+        /// <returns></returns>
+        public static string Clean(this string value, int length = 0)
+        {
+            // remover los espacios al inicio y final del string
+            var result = string.Empty;
+            if (!string.IsNullOrWhiteSpace(value))
+                result = value.Trim();
+
+            // truncar el string si length es mayor a cero
+            if (length > 0 && result.Length > length)
+                result = result.Substring(0, length);
+
+            // string procesado
+            return result;
+        }
+
+        /// <summary>
+        /// Limpiar, truncar y convertir un string a mayúsculas
+        /// </summary>
+        /// <param name="value">String a procesar</param>
+        /// <param name="length">Longitud a truncar el String</param>
+        /// <returns></returns>
+        public static string Upper(this string value, int length = 0)
+        {
+            // consumir función Clean() y convertir a mayúsculas
+            return value.Clean(length).ToUpper();
+        }
+
+        public static string Lower(this string value, int length = 0)
+        {
+            // consumir función Clean() y convertir a minúsculas
+            return value.Clean(length).ToLower();
+        }
     }
 }
